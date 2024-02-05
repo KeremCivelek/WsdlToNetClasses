238ad08eb3d5dd5a92c1024dcd0bd57036c57a53
keremcivelek <keremcivelek53@gmail.com>
2024-02-06 01:14:44 +0300

    comment

--- ApisGateway/Program.cs
+++ ApisGateway/Program.cs
@@ -2,10 +2,16 @@
 
 try
 {
+    //Wsdl Url
     string wsdlUrl = "http://www.dneonline.com/calculator.asmx?wsdl";
+    
+    //Svcutil.exe path for windows11
     string svcutilPath = @"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\svcutil.exe";
+    
+    //Output file path diffrence svcutil path
     string outputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "GeneratedClasses.cs");
 
+    //Start process on windows
     Process process = new Process();
     process.StartInfo.FileName = svcutilPath;
     process.StartInfo.Arguments = $"/language:C# /out:{outputFilePath} {wsdlUrl}";
