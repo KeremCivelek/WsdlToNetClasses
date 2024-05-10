7f038594c818e532dc3dd924603fa2d2eef1c467
keremcivelek <keremcivelek53@gmail.com>
2024-05-10 16:39:02 +0300

    tanımlama classları ve fonksiyonları ayırma işlemi

--- ApisGateway/Program.cs
+++ ApisGateway/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 try
 {
     //Wsdl Url
-    string wsdlUrl = "http://www.dneonline.com/calculator.asmx?wsdl";
+    string wsdlUrl = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL";
     
     //Svcutil.exe path for windows11
     string svcutilPath = @"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\svcutil.exe";
@@ -18,10 +19,156 @@ try
     process.Start();
     process.WaitForExit();
 
-    string classContent = File.ReadAllText(outputFilePath);
-    Console.WriteLine($"Created Class Path :{outputFilePath}");
-    Console.WriteLine("Created Class Content:");
-    Console.WriteLine(classContent);
+    #region MyRegion
+    //string classContent = File.ReadAllText(outputFilePath);
+    //Console.WriteLine($"Created Class Path :{outputFilePath}");
+    //Console.WriteLine("Created Class Content:");
+    //Console.WriteLine(classContent);
+
+    //// Sınıfları ve methodları ayırmak için düzenli ifade kullan
+    //string pattern = @"(public(?:\s+partial)?\s+class\s+.*?{(?:[^{}]+|(?<open>{)|(?<-open>}))*})";
+    //MatchCollection classMatches = Regex.Matches(classContent, pattern, RegexOptions.Singleline);
+    //// Sınıfları ve methodları tutacak bir liste oluştur
+    //List<string> classDefinitions = new List<string>();
+    //// Her bir eşleşme için sınıf tanımlamasını al ve listeye ekle
+    //foreach (Match match in classMatches)
+    //{
+    //    string classDefinition = match.Groups[1].Value;
+    //    classDefinitions.Add(classDefinition);
+    //}
+    //// Sınıf tanımlamalarını yazdır
+    //foreach (var classDef in classDefinitions)
+    //{
+    //    Console.WriteLine(classDef);
+    //}
+
+
+
+    //string[] lines = File.ReadAllLines(outputFilePath);
+    //// Tanımlama sınıflarını ve endpoint'leri ayırmak için Regex deseni
+    //string classPattern = @"public\s+(partial\s+)?class\s+\w+";
+    //string methodPattern = @"public\s+(\w+\s+)?\w+\s+\w+\s*\(.+\)";
+
+    //// Tanımlama sınıflarını ve endpoint'leri saklamak için listeler
+    //var classMatches = Regex.Matches(string.Join("\n", lines), classPattern);
+    //var methodMatches = Regex.Matches(string.Join("\n", lines), methodPattern);
+
+    //// Tanımlama sınıflarını ve endpoint'leri ekrana yazdır
+    //Console.WriteLine("Tanımlama Sınıfları:");
+    //foreach (Match match in classMatches)
+    //{
+    //    Console.WriteLine(match.Value);
+    //}
+
+    //Console.WriteLine("\nEndpoint'ler:");
+    //foreach (Match match in methodMatches)
+    //{
+    //    Console.WriteLine(match.Value);
+    //} 
+    #endregion
+
+
+    // Dosyadaki tüm satırları oku
+    string[] lines = File.ReadAllLines(outputFilePath);
+
+    #region MyRegion
+    //// Tanımlama sınıflarını ve endpoint'leri ayırmak için Regex deseni
+    //string classPattern = @"public\s+(partial\s+)?class\s+(\w+)";
+    //string propertyPattern = @"\b(public|private|protected|internal)\s+(\w+)\s+(\w+)\s*\{";
+
+    //// Sınıf adlarını ve property bilgilerini saklamak için sözlükler
+    //Dictionary<string, List<string>> classProperties = new Dictionary<string, List<string>>();
+
+    //// Sınıf adlarını bul
+    //var classMatches = Regex.Matches(string.Join("\n", lines), classPattern);
+    //foreach (Match classMatch in classMatches)
+    //{
+    //    string className = classMatch.Groups[2].Value;
+    //    classProperties[className] = new List<string>();
+    //}
+
+    //// Property bilgilerini bul
+    //var propertyMatches = Regex.Matches(string.Join("\n", lines), propertyPattern);
+    //foreach (Match propertyMatch in propertyMatches)
+    //{
+    //    string accessModifier = propertyMatch.Groups[1].Value;
+    //    string propertyType = propertyMatch.Groups[2].Value;
+    //    string propertyName = propertyMatch.Groups[3].Value;
+
+    //    // Property bilgisini ekleyerek sakla
+    //    string propertyInfo = $"{accessModifier} {propertyType} {propertyName}";
+    //    string className = classMatches
+    //        .Cast<Match>()
+    //        .FirstOrDefault(m => m.Index < propertyMatch.Index)?
+    //        .Groups[2]
+    //        .Value;
+
+    //    if (className != null && classProperties.ContainsKey(className))
+    //    {
+    //        classProperties[className].Add(propertyInfo);
+    //    }
+    //}
+
+    //// Sınıfların property bilgilerini ekrana yazdır
+    //foreach (var kvp in classProperties)
+    //{
+    //    Console.WriteLine($"Sınıf Adı: {kvp.Key}");
+    //    foreach (var propertyInfo in kvp.Value)
+    //    {
+    //        Console.WriteLine($"    {propertyInfo}");
+    //    }
+    //}
+
+    #endregion
+
+
+    // Tanımlama sınıflarını ve endpoint'leri ayırmak için Regex deseni
+    string classPattern = @"public\s+(partial\s+)?class\s+(\w+)";
+    string propertyPattern = @"\b(public|private|protected|internal)\s+(\w+)\s+(\w+)\s*\{";
+
+    // Sınıf adlarını ve property bilgilerini saklamak için sözlükler
+    Dictionary<string, List<string>> classProperties = new Dictionary<string, List<string>>();
+
+    // Sınıf adlarını bul
+    var classMatches = Regex.Matches(string.Join("\n", lines), classPattern);
+    foreach (Match classMatch in classMatches)
+    {
+        string className = classMatch.Groups[2].Value;
+        classProperties[className] = new List<string>();
+    }
+
+    // Property bilgilerini bul
+    foreach (var classMatch in classMatches)
+    {
+        string currentClass = ((Match)classMatch).Groups[2].Value;
+        string pattern = $@"{Regex.Escape(currentClass)}\s*{{|((?!{Regex.Escape(currentClass)}).)*\b(public|private|protected|internal)\s+(\w+)\s+(\w+)\s*\{{";
+        var propertyMatches = Regex.Matches(string.Join("\n", lines), pattern);
+
+        foreach (Match propertyMatch in propertyMatches)
+        {
+            if (propertyMatch.Groups[2].Success)
+            {
+                string accessModifier = propertyMatch.Groups[2].Value;
+                string propertyType = propertyMatch.Groups[3].Value;
+                string propertyName = propertyMatch.Groups[4].Value;
+
+                string propertyInfo = $"{accessModifier} {propertyType} {propertyName}";
+                classProperties[currentClass].Add(propertyInfo);
+            }
+        }
+    }
+
+    // Sınıfların property bilgilerini ekrana yazdır
+    foreach (var kvp in classProperties)
+    {
+        Console.WriteLine($"Sınıf Adı: {kvp.Key}");
+        foreach (var propertyInfo in kvp.Value)
+        {
+            Console.WriteLine($"    {propertyInfo}");
+        }
+    }
+
+
     Console.ReadLine();
 }
 catch (Exception ex)
---
+++ WsdlToStructs/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.ServiceModel;
+using System.Web.Services.Description;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace WsdlToStructsAndUse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // WSDL URL'si
+            string wsdlUrl = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL";
+
+            // WSDL dosyasını indirme veya URL'den alma
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead(wsdlUrl);
+
+            // ServiceDescription nesnesini oluştur
+            XmlSerializer ser = new XmlSerializer(typeof(Definitions));
+            Definitions definitions = (Definitions)ser.Deserialize(stream);
+
+            // WSDL'den tip bilgilerini al
+            XmlSchemaSet schemas = new XmlSchemaSet();
+            XmlSchemaImporter importer = new XmlSchemaImporter(schemas);
+
+            // ServiceContract, OperationContract ve DataContract tanımlarını bul
+            foreach (Message message in definitions.Items)
+            {
+                if (message is PortType portType)
+                {
+                    foreach (Operation operation in portType.Operation)
+                    {
+                        Console.WriteLine($"Operation Name: {operation.Name}");
+                        foreach (object input in operation.Item)
+                        {
+                            if (input is Input inputElement)
+                            {
+                                Console.WriteLine($"Input Message: {inputElement.Message.Name}");
+                            }
+                        }
+                    }
+                }
+                else if (message is Types types)
+                {
+                    foreach (object schema in types.Any)
+                    {
+                        XmlSchema xmlSchema = (XmlSchema)schema;
+                        schemas.Add(xmlSchema);
+                    }
+                }
+            }
+
+            // Schemas'ı kullanarak C# kodu oluştur
+            ServiceContractGenerator generator = new ServiceContractGenerator();
+            CodeGeneratorOptions options = new CodeGeneratorOptions();
+            options.BracingStyle = "C";
+
+            // Importer ile CodeDomProvider kullanarak kod birimini derle
+            CodeDomProvider provider = CodeDomProvider.CreateProvider("C#");
+            CodeNamespace codeNamespace = new CodeNamespace();
+            CodeCompileUnit codeCompileUnit = new CodeCompileUnit();
+            codeCompileUnit.Namespaces.Add(codeNamespace);
+
+            generator.GenerateServiceContractType(definitions, codeNamespace, codeCompileUnit);
+
+            // CodeDomProvider kullanarak kodu derle
+            StringWriter writer = new StringWriter();
+            provider.GenerateCodeFromCompileUnit(codeCompileUnit, writer, options);
+            string generatedCode = writer.ToString();
+
+            // Derlenmiş kodu çalışma zamanında yükle
+            Assembly assembly = Assembly.Load(generatedCode);
+
+            // Web servis istemci sınıfını al
+            Type serviceType = assembly.GetType("CountryInfoServiceSoapType");
+
+            // Web servisi çağır
+            MethodInfo[] methods = serviceType.GetMethods();
+            object serviceInstance = Activator.CreateInstance(serviceType);
+            foreach (var method in methods)
+            {
+                if (method.Name == "ListOfContinentsByName")
+                {
+                    var parameters = new object[] { true };
+                    var result = method.Invoke(serviceInstance, parameters);
+                    Console.WriteLine("Servis çağrısı başarılı. Sonuç: " + result);
+                    break;
+                }
+            }
+        }
+    }
+}
